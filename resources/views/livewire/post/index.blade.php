31cd1f5132b0372c3bc11c78504e38a16c038d07
amor <50753427+amordz72@users.noreply.github.com>
2022-01-17 20:42:25 +0100

    grid grid-cols-12 gap-1 mb-5

--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -3,23 +3,17 @@
     @section('title')
     | All post
     @endsection
-    <div class="grid">
-        <div class="col-spane-12">
-            col1
-        </div>
-        <div class="col-start-12">
-            <div>
-                data
-            </div>
 
-            <div class="flex justify-evenly"> {{ $posts->links() }}</div>
-        </div>
 
-    </div>
+    <div class="grid grid-cols-12 gap-1 mb-5">
+        <div class="col-start-0 col-span-4 bg-slate-400">01</div>
+        <div class="col-start-5 col-end-12 bg-red-400">02</div>
+
+      </div>
 
 
 
-
+    <div class="flex justify-evenly"> {{ $posts->links() }}</div>
 
 
 </div>
