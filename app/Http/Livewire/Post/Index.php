e50cc73a9bbfb3c6e1ead36d101175eb4c5fe5e3
amor <50753427+amordz72@users.noreply.github.com>
2022-01-17 15:03:18 +0100

    Pagination

--- app/Http/Livewire/Post/Index.php
+++ app/Http/Livewire/Post/Index.php
@@ -1,15 +1,27 @@
 <?php
 
 namespace App\Http\Livewire\Post;
+use Livewire\WithPagination;
 
 use Livewire\Component;
+use App\Models\User;
+use App\Models\Post;
 
 class Index extends Component
 {
+      use WithPagination;
+
 public $new=true;
-public $name="";
+public $all_posts;//
     public function render()
     {
-        return view('livewire.post.index');
+        return view('livewire.post.index', [
+            'posts' => Post::paginate(1),
+        ]);
+    }
+
+    public function mount()
+    {
+    $this->all_posts=Post::all();  //
     }
 }
--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -1,8 +1,16 @@
 <div class="mx-5">
   <h1 class="bg-red-500 text-center my-5">index</h1>
   <input type="text" wire:model.lazy='name'><br>
- <span>{{ $name }}</span> 
+
+
+<ul>
+  @foreach ($posts as $post)
+   <li class="bg-gray-300"><span>{{ $post->title }}</span></li>
+  @endforeach
+  </ul>
   <br>
+
+ <div class="flex justify-evenly"> {{ $posts->links() }}</div>
 
 
 </div>
