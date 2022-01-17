63ec96c6881da627dd0b9935401b22ebd3de2200
amor <50753427+amordz72@users.noreply.github.com>
2022-01-17 13:51:44 +0100

    npm run watch

--- app/Http/Livewire/Post/Index.php
+++ app/Http/Livewire/Post/Index.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class Index extends Component
 {
+public $new=true;
+public $name="";
     public function render()
     {
         return view('livewire.post.index');
--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -1,3 +1,5 @@
-<div>
-  <h1 class="bg-red-800">index</h1>
+<div class=" mx-5">
+  <h1 class="bg-red-500 text-center my-5">index</h1>
+  <input type="text" wire:model.lazy='name'><br>
+  {{ $name }}
 </div>
